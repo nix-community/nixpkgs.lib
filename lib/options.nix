nix-community/nixpkgs.lib

commit c563e02eb7e1096cf148cdaefdaddb4e058710f1

lib/options: Use escapeNixIdentifier for showOption

lib/options.nix
@@ -193,14 +193,7 @@ rec {
        (showOption ["foo" "bar" "baz"]) == "foo.bar.baz"
        (showOption ["foo" "bar.baz" "tux"]) == "foo.\"bar.baz\".tux"
   */
-  showOption = parts: let
-    escapeOptionPart = part:
-      let
-        escaped = lib.strings.escapeNixString part;
-      in if escaped == "\"${part}\""
-         then part
-         else escaped;
-    in (concatStringsSep ".") (map escapeOptionPart parts);
+  showOption = parts: concatMapStringsSep "." escapeNixIdentifier parts;
   showFiles = files: concatStringsSep " and " (map (f: "`${f}'") files);
   unknownModule = "<unknown-file>";
 

commit 21793034739166977ae41c112f0b2681370f3597

lib/options: Relax showOption quoting
https://github.com/NixOS/nixpkgs/commit/c563e02eb7e1096cf148cdaefdaddb4e058710f1
broke the build of NixOS manual.

It does not make sense to be as strict as with attributes since we
are not limited by the CLI's inability to handle numbers.
Placeholders should not be quoted either as they are not part of Nix
syntax but a meta-level construct.

lib/options.nix
@@ -192,8 +192,21 @@ rec {
      Example:
        (showOption ["foo" "bar" "baz"]) == "foo.bar.baz"
        (showOption ["foo" "bar.baz" "tux"]) == "foo.\"bar.baz\".tux"
+
+     Placeholders will not be quoted as they are not actual values:
+       (showOption ["foo" "*" "bar"]) == "foo.*.bar"
+       (showOption ["foo" "<name>" "bar"]) == "foo.<name>.bar"
+
+     Unlike attributes, options can also start with numbers:
+       (showOption ["windowManager" "2bwm" "enable"]) == "windowManager.2bwm.enable"
   */
-  showOption = parts: concatMapStringsSep "." escapeNixIdentifier parts;
+  showOption = parts:
+    let
+      escapeOptionPart = part:
+        if part == "*" || builtins.match "<.+>" part != null || builtins.match "[a-zA-Z0-9_][a-zA-Z0-9_'-]+" part != null
+        then part
+        else escapeNixIdentifier part;
+    in concatMapStringsSep "." escapeOptionPart parts;
   showFiles = files: concatStringsSep " and " (map (f: "`${f}'") files);
   unknownModule = "<unknown-file>";
 