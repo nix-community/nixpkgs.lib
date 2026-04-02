NixOS/nixpkgs

lib.types: add types.option
Signed-off-by: cinereal <cinereal@riseup.net>

lib/tests/modules/option.nix
@@ -1,0 +1,15 @@
+{ config, lib, ... }:
+{
+  options = {
+    theOption = lib.mkOption {
+      type = lib.types.option;
+    };
+    anOption = config.theOption;
+  };
+  config = {
+    theOption = lib.mkOption {
+      type = lib.types.int;
+    };
+    anOption = 10;
+  };
+}

nix-community/nixpkgs.lib

lib/tests/modules/option.nix
@@ -1,0 +1,15 @@
+{ config, lib, ... }:
+{
+  options = {
+    theOption = lib.mkOption {
+      type = lib.types.optionDeclaration;
+    };
+    anOption = config.theOption;
+  };
+  config = {
+    theOption = lib.mkOption {
+      type = lib.types.int;
+    };
+    anOption = 10;
+  };
+}