NixOS/nixpkgs

lib.licenses: add compound licenses

lib/licenses/licenses.nix
@@ -21,6 +21,7 @@ let
         deprecated
         redistributable
         ;
+      type = "simple";
     }
     // optionalAttrs (attrs ? spdxId) {
       inherit spdxId;

nix-community/nixpkgs.lib

lib/licenses/licenses.nix
@@ -21,6 +21,7 @@ let
         deprecated
         redistributable
         ;
+      licenseType = "simple";
     }
     // optionalAttrs (attrs ? spdxId) {
       inherit spdxId;